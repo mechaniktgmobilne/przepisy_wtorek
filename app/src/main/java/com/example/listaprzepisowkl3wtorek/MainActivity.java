961282306fff7fa776e5955f9b36b8bdb6ec62f8
mechaniktgmobilne <marzkup@gmail.com>
2025-12-02 08:09:41 +0100

    klikniecie elementu listview

--- app/src/main/java/com/example/listaprzepisowkl3wtorek/MainActivity.java
+++ app/src/main/java/com/example/listaprzepisowkl3wtorek/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.listaprzepisowkl3wtorek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listViewKategorie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewKategorie = findViewById(R.id.listViewKategorie);
+        listViewKategorie.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        view.setBackgroundColor(Color.GRAY);
+                        Toast.makeText(MainActivity.this,
+                                "Wybrano kategorię "+i,
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+        );
     }
 }
