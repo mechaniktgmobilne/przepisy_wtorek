d68728fa7be859d5089fdd6486892b9bcbde976b
mechaniktgmobilne <marzkup@gmail.com>
2025-12-09 07:43:13 +0100

    przpisy

--- app/src/main/java/com/example/listaprzepisowkl3wtorek/ListaPrzepisowActivity.java
+++ app/src/main/java/com/example/listaprzepisowkl3wtorek/ListaPrzepisowActivity.java
@@ -3,7 +3,11 @@ package com.example.listaprzepisowkl3wtorek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ListaPrzepisowActivity extends AppCompatActivity {
 
@@ -15,5 +19,16 @@ public class ListaPrzepisowActivity extends AppCompatActivity {
                 .getStringExtra("KATEGORIA");
         Toast.makeText(this, kategoriaPrzepisu,
                 Toast.LENGTH_SHORT).show();
+        //TODO :zrobić poniższe jeszcze raz
+        ArrayList<Przepis> przepisy
+                = RepozytoriumPrzepisow.getPrzepisy();
+        //TODO: wyswietlic tylko przpisy z tej kategorii
+        ListView listView = findViewById(R.id.listViewListaPrzepisow);
+        ArrayAdapter<Przepis> arrayAdapter = new ArrayAdapter<>(
+                ListaPrzepisowActivity.this,
+                android.R.layout.simple_list_item_1,
+                przepisy
+        );
+        listView.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/listaprzepisowkl3wtorek/RepozytoriumPrzepisow.java
+++ app/src/main/java/com/example/listaprzepisowkl3wtorek/RepozytoriumPrzepisow.java
@@ -1,4 +1,26 @@
 package com.example.listaprzepisowkl3wtorek;
-public class RepozytoriumPrzepisow {
 
+import java.util.ArrayList;
+
+public class RepozytoriumPrzepisow {
+    private static ArrayList<Przepis> przepisy;
+    private static void generujPrzepisy(){
+        przepisy = new ArrayList<>();
+        przepisy.add(new Przepis("Pierniczki","ciasteczka",
+                R.drawable.pierniczki,
+                "mąka, kakao, przyprawy, miód",
+                "wszystko wymieszać"));
+        przepisy.add(new Przepis("Muffinki","ciasteczka",R.drawable.muffinka,"mąka, mleko, kakao","wszystko wymieszać, upiec"));
+        przepisy.add(new Przepis("Przepis2"));
+        przepisy.add(new Przepis("Przepis3"));
+        przepisy.add(new Przepis("Przepis4"));
+        przepisy.add(new Przepis("Sernik na zimno", "ciasta",R.drawable.sernik,"ser biały, galaretka,woda","wymieszaj i do lodówki"));
+        przepisy.add(new Przepis("Herbata zimowa","napoje",R.drawable.herbata_zimowa,"herbata, rozmaryn, gożdziki,jabłko, pomarańcza,imbir","wszystko zalać wrzątkiem i poczekać"));
+
+    }
+
+    public static ArrayList<Przepis> getPrzepisy(){
+        generujPrzepisy();
+        return przepisy;
+    }
 }
